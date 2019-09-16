11a7e6e5eb0d9cd585325a5372e2783dc25651bb
Kyle DeBry <debry.1@osu.edu>
2019-09-16 10:30:50 -0400

    Sim 3: First absorption attempt

--- transmittance.py
+++ transmittance.py
@@ -20,9 +20,9 @@ class ZeroNormalize(pltcolors.Normalize):
 
 
 def main():
-    duration = 60
-    resolution = 5
-    cell_x = 30
+    duration = 240
+    resolution = 6
+    cell_x = 120
     cell_y = 70
     cell_z = 110
     pml = 2
@@ -83,11 +83,11 @@ def main():
     fr_y = max(min(cladding_thickness, cell_y - 2 * pml), 0)
     fr_z = max(min(cladding_thickness, cell_z - 2 * pml), 0)
 
-    refl_fr = mp.FluxRegion(center=mp.Vector3(-0.5 * cell_x + pml + src_buffer + 1, 0, 0),
+    refl_fr = mp.FluxRegion(center=mp.Vector3(-0.5 * cell_x + pml + 2 * src_buffer, 0, 0),
                             size=mp.Vector3(0, fr_y, fr_z))
     refl = sim.add_flux(freq, 0, 1, refl_fr)
 
-    tran_fr = mp.FluxRegion(center=mp.Vector3(0.5 * cell_x - pml - 1, 0, 0),
+    tran_fr = mp.FluxRegion(center=mp.Vector3(0.5 * cell_x - pml - src_buffer, 0, 0),
                             size=mp.Vector3(0, fr_y, fr_z))
     tran = sim.add_flux(freq, 0, 1, tran_fr)
 
@@ -97,32 +97,30 @@ def main():
                                         mp.at_every(0.25, mp.output_efield_z))),
             until=duration)
 
+    # for normalization run, save flux fields data for reflection plane
+    no_absorber_refl_data = sim.get_flux_data(refl)
+    # save incident power for transmission plane
+    no_absorber_tran_flux = mp.get_fluxes(tran)
+
+    print("transmitted: {}".format(no_absorber_tran_flux))
 
     eps_data = sim.get_array(center=mp.Vector3(), size=mp.Vector3(cell_x, cell_y, 0), component=mp.Dielectric)
 
     max_field = 0.1
 
-    print("Data collected")
+    if mp.am_master():
+        plt.figure()
+        plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+        plt.axis('off')
+        plt.savefig('Sim3_Eps_A.png', dpi=300)
 
-    # if mp.am_master():
-    #     print("I am master")
-    #     plt.figure()
-    #     plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
-    #     plt.axis('off')
-    #     plt.show()
-    #     print("Plotted")
-    #
-    # cm = pltcolors.LinearSegmentedColormap.from_list(
-    #         'em', [(0,0,1), (0,0,0), (1,0,0)])
-    #
     ez_data = sim.get_array(center=mp.Vector3(), size=mp.Vector3(cell_x, cell_y, 0), component=mp.Ez)
     if mp.am_master():
         plt.figure()
         plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
         plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
         plt.axis('off')
-        plt.show()
-        print("Plotted 2")
+        plt.savefig('Sim3_Ez_A.png', dpi=300)
 
     eps_cross_data = sim.get_array(center=mp.Vector3(x=cell_x/4), size=mp.Vector3(0, cell_y, cell_z), component=mp.Dielectric)
     num_x = 4
@@ -130,71 +128,90 @@ def main():
     fig, ax = plt.subplots(num_x, num_y)
     fig.suptitle('Cross Sectional Ez Fields')
     for i in range(num_x * num_y):
-        ez_cross_data = sim.get_array(center=mp.Vector3(x=cell_x/4 + i / resolution / 2), size=mp.Vector3(0, cell_y, cell_z), component=mp.Ez)
+        monitor_x = cell_x/4 + i / (wavelength / 1.444) / 4
+        ez_cross_data = sim.get_array(center=mp.Vector3(x=monitor_x), size=mp.Vector3(0, cell_y, cell_z), component=mp.Ez)
         ax_num = i // num_y, i % num_y
         if mp.am_master():
             ax[ax_num].imshow(eps_cross_data, interpolation='spline36', cmap='binary')
             ax[ax_num].imshow(ez_cross_data, interpolation='spline36', cmap='RdBu', alpha=0.9, norm=ZeroNormalize(vmax=np.max(max_field)))
             ax[ax_num].axis('off')
-            ax[ax_num].set_title('x = {}'.format(cell_x/4 + i / resolution))
+            ax[ax_num].set_title('x = {}'.format(round(cell_x/4 + i / resolution, 3)))
             # ax[i].show()
             print("Plotted 3-{}".format(i))
     if mp.am_master():
-        plt.show()
-    # for normalization run, save flux fields data for reflection plane
-    no_absorber_refl_data = sim.get_flux_data(refl)
-    # save incident power for transmission plane
-    no_absorber_tran_flux = mp.get_fluxes(tran)
+        plt.savefig('Sim3_Ez_CS_A.png', dpi=300)
 
-    # sim.reset_meep()
-    #
-    # geometry.append(absorber)
-    #
-    # sim = mp.Simulation(cell_size=cell,
-    #                     boundary_layers=pml_layers,
-    #                     geometry=geometry,
-    #                     sources=sources,
-    #                     resolution=resolution,
-    #                     eps_averaging=False,
-    #                     default_material=default_material,
-    #                     symmetries=[mp.Mirror(mp.Z)])
-    #
-    # refl = sim.add_flux(freq, 0, 1, refl_fr)
-    # tran = sim.add_flux(freq, 0, 1, tran_fr)
-    #
-    # sim.load_minus_flux_data(refl, no_absorber_refl_data)
 
-    # sim.run(mp.at_beginning(mp.output_epsilon),
-    #         mp.to_appended("ez_z0",
-    #                        mp.in_volume(output_slice,
-    #                                     mp.at_every(0.25, mp.output_efield_z))),
-    #         until=duration)
-    #
-    # absorber_refl_flux = mp.get_fluxes(refl)
-    # absorber_tran_flux = mp.get_fluxes(tran)
-    #
-    # transmittance = absorber_tran_flux[0] / no_absorber_tran_flux[0]
-    # reflectance = absorber_refl_flux[0] / no_absorber_tran_flux[0]
-    # absorption = 1 - transmittance
-    # penetration_depth = - mosi_length / math.log(transmittance)
-    #
-    # print("Transmittance: %f" % transmittance)
-    # print("Reflectance: %f" % reflectance)
-    # print("Absorption: {} over {} um".format(absorption, mosi_length))
-    # print("lambda = {} mm".format(penetration_depth / 1000))
-    #
-    # eps_data = sim.get_array(center=mp.Vector3(), size=mp.Vector3(cell_x, cell_y, 0), component=mp.Dielectric)
-    # plt.figure()
-    # plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
-    # plt.axis('off')
-    # plt.show()
-    #
-    # cm = pltcolors.LinearSegmentedColormap.from_list(
-    #         'em', [(0,0,1), (0,0,0), (1,0,0)])
-    #
-    # ez_data = sim.get_array(center=mp.Vector3(), size=mp.Vector3(cell_x, cell_y, 0), component=mp.Ez)
-    # plt.figure()
-    # plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
-    # plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
-    # plt.axis('off')
-    # plt.show()
+    sim.reset_meep()
+
+    geometry.append(absorber)
+
+    sim = mp.Simulation(cell_size=cell,
+                        boundary_layers=pml_layers,
+                        geometry=geometry,
+                        sources=sources,
+                        resolution=resolution,
+                        eps_averaging=False,
+                        default_material=default_material,
+                        symmetries=[mp.Mirror(mp.Z, phase=-1)])
+
+    refl = sim.add_flux(freq, 0, 1, refl_fr)
+    tran = sim.add_flux(freq, 0, 1, tran_fr)
+
+    sim.load_minus_flux_data(refl, no_absorber_refl_data)
+
+    sim.run(mp.at_beginning(mp.output_epsilon),
+            mp.to_appended("ez_z0",
+                           mp.in_volume(output_slice,
+                                        mp.at_every(0.25, mp.output_efield_z))),
+            until=duration)
+
+    absorber_refl_flux = mp.get_fluxes(refl)
+    absorber_tran_flux = mp.get_fluxes(tran)
+
+    transmittance = absorber_tran_flux[0] / no_absorber_tran_flux[0]
+    reflectance = absorber_refl_flux[0] / no_absorber_tran_flux[0]
+    absorption = 1 - transmittance
+    penetration_depth = - mosi_length / math.log(transmittance)
+
+    print("Transmittance: %f" % transmittance)
+    print("Reflectance: %f" % reflectance)
+    print("Absorption: {} over {} um".format(absorption, mosi_length))
+    print("lambda = {} mm".format(penetration_depth / 1000))
+
+    eps_data = sim.get_array(center=mp.Vector3(), size=mp.Vector3(cell_x, cell_y, 0), component=mp.Dielectric)
+
+    max_field = 0.1
+
+    if mp.am_master():
+        plt.figure()
+        plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+        plt.axis('off')
+        plt.savefig('Sim3_Eps_B.png', dpi=300)
+
+    ez_data = sim.get_array(center=mp.Vector3(), size=mp.Vector3(cell_x, cell_y, 0), component=mp.Ez)
+    if mp.am_master():
+        plt.figure()
+        plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+        plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
+        plt.axis('off')
+        plt.savefig('Sim3_Ez_B.png', dpi=300)
+
+    eps_cross_data = sim.get_array(center=mp.Vector3(x=cell_x/4), size=mp.Vector3(0, cell_y, cell_z), component=mp.Dielectric)
+    num_x = 4
+    num_y = 3
+    fig, ax = plt.subplots(num_x, num_y)
+    fig.suptitle('Cross Sectional Ez Fields')
+    for i in range(num_x * num_y):
+        monitor_x = cell_x/4 + i / (wavelength / 1.444) / 4
+        ez_cross_data = sim.get_array(center=mp.Vector3(x=monitor_x), size=mp.Vector3(0, cell_y, cell_z), component=mp.Ez)
+        ax_num = i // num_y, i % num_y
+        if mp.am_master():
+            ax[ax_num].imshow(eps_cross_data, interpolation='spline36', cmap='binary')
+            ax[ax_num].imshow(ez_cross_data, interpolation='spline36', cmap='RdBu', alpha=0.9, norm=ZeroNormalize(vmax=np.max(max_field)))
+            ax[ax_num].axis('off')
+            ax[ax_num].set_title('x = {}'.format(round(cell_x/4 + i / resolution, 3)))
+            # ax[i].show()
+            print("Plotted 3-{}".format(i))
+    if mp.am_master():
+        plt.savefig('Sim3_Ez_CS_B.png', dpi=300)
